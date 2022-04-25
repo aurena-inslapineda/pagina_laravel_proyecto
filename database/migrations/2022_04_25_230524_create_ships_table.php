f137bbd9f9201e147238b68e97efe4e5157eecc1
Adrian Ureña <adrian.urena@laptop>
2022-04-26 01:07:23 +0200

    Added ships database

---
+++ database/migrations/2022_04_25_230524_create_ships_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ships', function (Blueprint $table) {
+            $table->id();
+            $table->string('ship_name')->unique();
+            $table->unsignedBigInteger('manufacturer_id');
+            $table->unsignedBigInteger('rol_id');
+            $table->unsignedBigInteger('focus_id');
+            $table->string('ship_image');
+            $table->tinyInteger('crew_size');
+            $table->float('lenght');
+            $table->integer('mass');
+            $table->timestamps();
+
+
+            $table->foreign('manufacturer_id')->references('id')->on('manufacturers')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('rol_id')->references('id')->on('rol')->onDelete('cascade')->onUpdate('cascade');
+            $table->foreign('focus_id')->references('id')->on('focus')->onDelete('cascade')->onUpdate('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('ships');
+    }
+};
